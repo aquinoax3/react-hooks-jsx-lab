a07000edf0e389dda5d59c336182298763f81c2a
Axel Aquino <aquino.axel@gmail.com>
2022-02-21 23:23:31 -0500

    commit tree 910c58a2dc04b49d870333202f5c21fb9dfae865

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { image } from "../data/data";
 
+const h2 = <h2>About Me</h2>;
+const p = <p>What me get this!!!</p>;
+const img = <img src={image} alt="I made this"></img>;
+
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      About
+      {h2}
+      {p}
+      {img}
+    </div>
+  );
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+const home = <a href="#home">Home</a>;
+const about = <a href="#about">About</a>;
+
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+    <nav>
+      NavBar
+      {home}
+      {about}
+    </nav>
+  );
 }
 
 export default NavBar;
